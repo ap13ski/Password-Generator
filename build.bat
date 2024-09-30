288a44d980dc572272911e43588c6ad04a4c2af3
ap13ski <ap13ski@yandex.ru>
2024-09-30 22:01:57 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 rem MinGW-W64 v.8.1.0
 windres resources.rc -o resources.o
-g++ -static main.cpp clipboard.cpp password.cpp resources.o -lgdi32 -luser32 -lkernel32 -lcomctl32 -lgcc -lstdc++ -mwindows
+g++ main.cpp clipboard.cpp password.cpp resources.o -static -lgdi32 -luser32 -lkernel32 -lcomctl32 -lgcc -lstdc++ -mwindows
 
 pause
