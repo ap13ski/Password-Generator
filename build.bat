976974269b3cb18348ee39c5bfa7113d4c9bd6a4
ap13ski <ap13ski@yandex.ru>
2025-03-13 16:03:17 +0300

    Delete build.bat

--- build.bat
+++
@@ -1,5 +0,0 @@
-rem MinGW-W64 v.8.1.0
-windres resources.rc -o resources.o
-g++ main.cpp clipboard.cpp password.cpp resources.o -static -lgdi32 -luser32 -lkernel32 -lcomctl32 -lgcc -lstdc++ -mwindows
-
-pause
