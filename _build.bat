ae87492586b9da45783c4b015471726b52e51dbb
ap13ski <ap13ski@yandex.ru>
2025-03-15 13:03:04 +0300

    Fixed: tabs & spaces alignments

--- _build.bat
+++ _build.bat
@@ -2,4 +2,6 @@ rem MinGW resource compiler
 windres resources.rc -o resources.o
 g++ main.cpp clipboard.cpp password.cpp resources.o -static -lgdi32 -luser32 -lkernel32 -lcomctl32 -lgcc -lstdc++ -mwindows
 
+del *.o
+
 pause
