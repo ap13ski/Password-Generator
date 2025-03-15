51e71b43e465c60b0d535a1c08adf81e46105c5a
ap13ski <ap13ski@yandex.ru>
2025-03-15 13:40:08 +0300

    Add files via upload

--- _build.bat
+++ _build.bat
@@ -1,6 +1,6 @@
 rem MinGW resource compiler
 windres resources.rc -o resources.o
-g++ main.cpp clipboard.cpp password.cpp resources.o -static -lgdi32 -luser32 -lkernel32 -lcomctl32 -lgcc -lstdc++ -mwindows
+g++ main.cpp clipboard.cpp password.cpp resources.o -static -lgdi32 -luser32 -lkernel32 -lcomctl32 -lgcc -lstdc++ -mwindows -o password_generator_x64.exe
 
 del *.o
 
